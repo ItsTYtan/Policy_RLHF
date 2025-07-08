969bce754694ac108645429530f1c82fb092f2fe
ItsTYtan <tytan216@gmail.com>
2025-07-08 11:03:26 +0800

    small udpate

--- model_tohf.py
+++ model_tohf.py
@@ -7,7 +7,7 @@ from huggingface_hub import create_branch, create_tag
 
 directory = '/home/tytan216/volume/tzeyoung/Policy_RLHF/models/Qwen/Qwen2.5-1.5B-Instruct-SFT' 
 
-for f in os.listdir(directory):
+for f in sorted(os.listdir(directory), reverse=True):
     print(f)
     create_branch("ItsTYtan/Qwen2.5_1.5b_policytuned", repo_type="model", branch=f, exist_ok=True)
 
